fabba2c97993c9227d9a4d4895e147cb0b81aa92
LakshmiShiva <49052619+LakshmiShiva@users.noreply.github.com>
2019-05-24 14:51:47 +0530

    Update TestRunner.java

--- src/test/java/org/stepdefinition/TestRunner.java
+++ src/test/java/org/stepdefinition/TestRunner.java
@@ -4,7 +4,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="C:\\UsersLakshmi\\CloneTry\\eclipse-workspace\\cucumber\\src\\test\\resources\\feature\\pay.feature",dryRun=false,glue= {"org.stepdefinition"}, plugin=  {"json:src/test/java/org.stepdefinition/report.json"})
+@CucumberOptions(features="C:\\UsersLakshmi\\CloneTry\\eclipse-workspace\\cucumber\\src\\test\\resources\\feature\\pay.feature",dryRun=false,glue= {"org.stepdefinition"}, plugin= {"json:src/test/java/org.stepdefinition/report.json"})
 public class TestRunner {
 
 }
